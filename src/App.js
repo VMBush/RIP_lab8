366ae61a3281cf4fd9ccc6336f3a7b5cb4f22d7d
VMBush <71069537+VMBush@users.noreply.github.com>
2022-01-20 22:50:24 +0300

    finita la comedia 2

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import React, {useEffect, useState} from 'react';
 import {Card, Col, Row, Button, Spinner} from "react-bootstrap";
 import VideoCard from './components/VideoCard.js'
 import DetailCard from "./components/DetailCard";
+import "./App.css"
 
 
 
-const getVideosByName = async (name = 'пуп') =>{
+const getVideosByName = async (name = '') =>{
     const res = await fetch(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyD4ECa0a1mGKr6Psf1GYrcuExIDppu2XmU&part=snippet&q=${name}&chart=mostPopular&maxResults=10`)
         .then((response) => {
             return response.json();
@@ -18,7 +19,7 @@ const getVideosByName = async (name = 'пуп') =>{
 
 function App() {
 
-    const [searchValue, setSearchValue] = useState('пуп');
+    const [searchValue, setSearchValue] = useState('лягушки');
 
     const [loading, setLoading] = useState(false)
 
@@ -34,9 +35,9 @@ function App() {
     return (
         <BrowserRouter basename="/" >
             <div>
-                <ul>
+                <ul className='head'>
                     <li>
-                        <Link to="/">Главная страница</Link>
+                        <Link  to="/">Главная страница</Link>
                     </li>
                     <li>
                         <Link to="/search">Страница поиска</Link>
@@ -45,18 +46,18 @@ function App() {
                 <hr />
                 <Switch>
                     <Route exact path="/">
-                        <h1>Это наша стартовая страница</h1>
+                        <h1 className="head">Добро пожаловать на страницу поиска чудесных видеозаписей про лягушек и не только</h1>
                     </Route>
                     <Route path="/search">
 
                         <div className={`container ${loading && 'containerLoading'}`}>
                             {loading && <div className="loadingBg"><Spinner animation="border"/></div>}
-                            <div className="inputField">
-                                <input value={searchValue} onChange={(event => setSearchValue(event.target.value))}/>
-                                <Button disabled={loading} onClick={handleSearch}>Искать</Button>
+                            <div className="input">
+                                <input className="inputField" value={searchValue} onChange={(event => setSearchValue(event.target.value))}/>
+                                <Button className="inputField" disabled={loading} onClick={handleSearch}>Искать</Button>
                             </div>
                             {!videos.length && <div>
-                                <h1>К сожалению, пока ничего не найдено :(</h1>
+                                <h1 className="search-error">К сожалению, пока ничего не найдено :(</h1>
                             </div>}
 
                             <Row xs={4} md={4} className="g-4">
